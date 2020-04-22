2c888ce9cee4607394361d3354d89127897c3809
PageSeanA <pageseana@gmail.com>
2020-04-22 13:22:52 -0500

    test

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,7 +30,7 @@ export default function HomeScreen() {
           </View>
 
           <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload. SEAN ALINA diego
+            Change any of the text, save the file, and your app will automatically reload. HELLO SEAN ALINA DIEGO!! シ
           </Text>
         </View>
 
